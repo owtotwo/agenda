3e39ba9babd12588b7c3da9a177d241a67585b6c
owtotwo <owtotwo@163.com>
2018-11-08 22:34:20 +0800

    可以跑起来了

---
+++ cmd/root.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var RootCmd = &cobra.Command{
+	Use:   "agenda",
+	Short: "Agenda system rewritten with golang.",
+	Long: `Service calculating homework for week 6 & 7. 
+This commline tools will help you manage your meetings. For more detail, 
+please use \'agenda help\' to begin.`,
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() { 
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Search config in home directory with name ".tmp" (without extension).
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".tmp")
+
+	viper.AutomaticEnv()
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "fmt"
+import "github.com/owtotwo/agenda/cmd"
 
 func main() {
-    fmt.Printf("hello, agenda\n")
+	cmd.Execute()
 }
